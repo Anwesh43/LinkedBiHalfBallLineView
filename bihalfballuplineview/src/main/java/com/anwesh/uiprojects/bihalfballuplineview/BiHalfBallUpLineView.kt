dc25a5cdc005a0e95744f4586b125eac9d467bf2
Anwesh43 <anweshthecool0@gmail.com>
2020-05-23 04:31:39 +0530

    adding constants and libraries

--- bihalfballuplineview/src/main/java/com/anwesh/uiprojects/bihalfballuplineview/BiHalfBallUpLineView.kt
+++ bihalfballuplineview/src/main/java/com/anwesh/uiprojects/bihalfballuplineview/BiHalfBallUpLineView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.bihalfballuplineview
 /**
  * Created by anweshmishra on 23/05/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
