f95b8bd94d4bf31acce4900537054892c9892e95
Anwesh43 <anweshthecool0@gmail.com>
2020-05-23 04:52:59 +0530

    adding BiHalfBallUpLineView class

--- bihalfballuplineview/src/main/java/com/anwesh/uiprojects/bihalfballuplineview/BiHalfBallUpLineView.kt
+++ bihalfballuplineview/src/main/java/com/anwesh/uiprojects/bihalfballuplineview/BiHalfBallUpLineView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawBHULNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class BiHalfBallUpLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
